b6a20e96ec35191a355756d0d0ad2c2e5b58847e
Jared Galloway <jaredgalloway07@gmail.com>
2022-05-18 17:10:14 -0700

    Rhino config (#3)
    * Add rhino.config
    * Add run_rhino,sh
    
    Co-authored-by: Mathieu Fourment <m.fourment@gmail.com>

---
+++ run_rhino.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+source /app/lmod/lmod/init/profile
+
+module load nextflow
+module load Singularity
+export PATH=$SINGULARITYROOT/bin/:$PATH
+
+/usr/bin/time nextflow  \
+    -C ./configs/rhino.config \
+    run main.nf \
+    --results "$(date -I)-rhino-results" \
+    -profile rhino \
+    -with-report ./r_output/nextflow_report.html \
+    -work-dir ./r_output/work/ \
+    -resume
